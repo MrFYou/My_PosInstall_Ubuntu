a0bb7ea58fd783756ae63d7814f3473256ece3b7
Isaias Rosa <isaiasrosa9@outlook.com>
2020-03-20 09:43:20 -0400

    add synaptic e usb-creator-gtk

--- post-install-ubuntu.sh
+++ post-install-ubuntu.sh
@@ -42,6 +42,8 @@ sudo apt install cinnamon -y
 sudo apt install xed -y
 sudo apt install redshift redshift-gtk -y
 sudo apt install lightdm-settings slick-greeter -y
+sudo apt install synaptic -y
+sudo apt install usb-creator-gtk -y ##Criador de discos de inicalização##
 sudo apt install msa-daemon msa-ui-qt mcpelauncher-client mcpelauncher-ui-qt libegl1-mesa:i386 libegl1-mesa-dev:i386 -y ##Minecraft-bedrock##
 sudo apt install p7zip p7zip-full p7zip-rar -y ##Suporte a arquivos .7z e .rar##
 
