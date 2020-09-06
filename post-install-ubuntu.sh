d2cab6c5e646d2c6e5b466c8b5d9cfa49ade2fd6
Fernando Rosa <fernandorosa9@outlook.com>
2020-09-06 15:44:06 -0400

    Atualizado

--- post-install-ubuntu.sh
+++ post-install-ubuntu.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 ##--------------------------------------------------- VARIÁVEIS -------------------------------------------------##
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 URL_STEAM="https://steamcdn-a.akamaihd.net/client/installer/steam.deb"
 URL_MINECRAFT="https://launcher.mojang.com/download/Minecraft.deb"
 URL_TELEGRAM="https://telegram.org/dl/desktop/linux"
 URL_TEMAS_MINT="https://github.com/MrFYou/Themes_of_Linux_Mint/archive/master.zip"
-URL_DISCORD="https://dl.discordapp.net/apps/linux/0.0.10/discord-0.0.10.deb"
-URL_STREMIO="https://dl.strem.io/linux/v4.4.106/stremio_4.4.106-1_amd64.deb"
-DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
-mkdir "$DIRETORIO_DOWNLOADS"
+URL_DISCORD="https://dl.discordapp.net/apps/linux/0.0.11/discord-0.0.11.deb"
+URL_STREMIO="https://dl.strem.io/shell-linux/v4.4.116/stremio_4.4.116-1_amd64.deb"
+URL_VIRUTALBOX="https://download.virtualbox.org/virtualbox/6.1.14/virtualbox-6.1_6.1.14-140239~Ubuntu~eoan_amd64.deb"
+URL_VISUALSTUDIOCODE="https://az764295.vo.msecnd.net/stable/a0479759d6e9ea56afa657e454193f72aef85bd0/code_1.48.2-1598353430_amd64.deb"
 ## --------------------------------------------------------------------------------------------------------------------##
+
+mkdir "$DIRETORIO_DOWNLOADS"
 
 ## Removendo travas eventuais do apt ##
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
 ## Adicionando arquitetura de 32 bits ##
+sudo dpkg --add-architecture i386
 
 ##Adicionando repositorios e PPAs##
-sudo add-apt-repository ppa:obsproject/obs-studio -y
+
 
 ##Removendo pacotes pre-instalados##
+
+
 
 ##Escolhendo interface##
 aux=1
@@ -31,7 +37,7 @@ do
     case "$resposta" in
      "Ubuntu"|"ubuntu")
               aux=$[ $aux - 1]
-              sudo apt install gnome-software gnome-tweaks -y
+              sudo apt install gnome-software gnome-tweaks gnome-shell-extensions -y
               sudo snap remove snap-store
      ;;
       "Gnome"|"gnome")
@@ -63,6 +69,21 @@ wget -c "$URL_STEAM"       -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_MINECRAFT"       -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_DISCORD"       -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_STREMIO"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_VIRUTALBOX"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_VISUALSTUDIOCODE"       -P "$DIRETORIO_DOWNLOADS"
+
+##OBS Studio##
+sudo add-apt-repository ppa:obsproject/obs-studio -y
+sudo apt update
+sudo apt install obs-studio ffmpeg -
+
+
+##WINE##
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
+sudo apt update
+sudo apt install --install-recommends winehq-stable -y
 
 
 ##Instalando pacotes apt##
@@ -72,9 +93,8 @@ sudo apt install git -y
 sudo apt install gedit -y
 sudo apt install transmission -y
 sudo apt install nano -y
+sudo apt install curl -y
 sudo apt install synaptic -y
-sudo apt install virtualbox -y
-sudo apt install obs-studio ffmpeg -y
 sudo apt install p7zip p7zip-full p7zip-rar -y ##Suporte para arquivos .7z e .rar##
 
 ## Instalando pacotes .deb baixados##
@@ -83,36 +103,35 @@ sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
 ##Consertando dependencias faltando/quebradas##
 sudo apt --fix-broken install -y
 
-##Instalando Telegram##
+##Telegram##
 cd "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_TELEGRAM" -O telegram.tar.xz
 sudo tar Jxf $DIRETORIO_DOWNLOADS/telegram.tar.xz -C /opt/
 sudo ln -sf /opt/Telegram/Telegram /usr/bin/Telegram
-##echo -e '[Desktop Entry]\n Version=1.0\n Exec=/opt/Telegram/Telegram\n Name=Telegram\n Icon=Telegram\n Type=Application\n Categories=Application;Network;' | sudo tee /usr/share/applications/telegram.desktop##
-##sudo chmod +x /usr/share/applications/telegram.desktop##
+echo -e '[Desktop Entry]\n Version=1.0\n Exec=/opt/Telegram/Telegram\n Name=Telegram\n Icon=Telegram\n Type=Application\n Categories=Application;Network;' | sudo tee /usr/share/applications/telegram.desktop
+sudo chmod +x /usr/share/applications/telegram.desktop
 cd ~
 
-##Instalando asdf-vm##
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.7
+##asdf-vm##
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0-rc1
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 sudo apt install automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev unzip curl -y
 
-## Instalando pacotes Snap ##
+##Instalando pacotes Snap ##
 sudo snap install spotify
 sudo snap install photogimp
-sudo snap install code --classic ##Visual-Studio-Code##
 sudo snap install authy --beta
 
-## Instalando pacotes Flatpak ##
+##Instalando pacotes Flatpak ##
 sudo flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref -y ##GIMP##
-sudo flatpak install flathub org.videolan.VLC ##VLC##
+sudo flatpak install flathub org.videolan.VLC -y ##VLC##
 
 ##Limpar e atualizar##
 sudo rm -r $DIRETORIO_DOWNLOADS
 sudo apt update
-sudo apt autoremove -y
 sudo apt upgrade -y
+sudo apt autoremove -y
 
 ##reiniciar##
 aux=1
